186045a40bed428ac4102e0190055ef3fb41340d
Temiloluwa Adeleye <temiloluwagithub@gmail.com>
2023-10-03 18:11:07 +0100

    intersection observer

--- main.js
+++ main.js
@@ -1,24 +1,62 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+const the_animation = document.getElementsByClassName("animation");
+const sections = document.getElementsByTagName("section");
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+let options = {
+  root: document.querySelector("body"),
+  rootMargin: "0px",
+  threshold: 0.1,
+};
 
-setupCounter(document.querySelector('#counter'))
+const sectionObserver = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      console.log(entry);
+      entry.target.style.backgroundColor = "green";
+    } else {
+      entry.target.style.backgroundColor = "white";
+    }
+  });
+}, options);
+
+for (let i = 0; i < sections.length; i++) {
+  const element = sections[i];
+  sectionObserver.observe(element);
+}
+
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("scroll-animation");
+      } else {
+        entry.target.classList.remove("scroll-animation");
+      }
+    });
+  },
+  { threshold: 1 }
+);
+
+for (let i = 0; i < the_animation.length; i++) {
+  const elements = the_animation[i];
+  observer.observe(elements);
+}
+
+const [isVisible, setIsVisible] = useState(false);
+const articleRef = useRef<HTMLTitleElement | null>(null);
+
+//react js intersection observer code.
+useEffect(() => {
+  const observer = new IntersectionObserver(
+    (entry) => {
+      entry.forEach((entry) => {
+        setIsVisible(entry.isIntersecting);
+      });
+    },
+    { threshold: 1 }
+  );
+
+  console.log(isVisible)
+  if(articleRef.current){
+    observer.observe(articleRef.current)
+  }
+}, [articleRef, isVisible]);
